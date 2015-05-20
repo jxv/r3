1feaa7d0a406b1a6f24c050ffadfb31e795ec65a
jxv <jxv@hush.com>
2015-05-19 21:29:01 -0700

    Move out cuboid_spec to spec.c

--- makefile
+++ makefile
@@ -7,6 +7,7 @@ all:
 	xxd -i shader/texture.vert > shader/texture.vert.h
 	xxd -i shader/texture.frag > shader/texture.frag.h
 	gcc src/r3.c -c -o src/r3.o -I./include -Wall -Werror -pedantic -std=c11 -ffast-math -g -O3 -lml -lm -lc -D_GNU_SOURCE
+	gcc src/spec.c -c -o src/spec.o -I./include -Wall -Werror -pedantic -std=c11 -ffast-math -g -O3 -lml -lm -lc -D_GNU_SOURCE
 	ar rvs libr3.a src/*.o
 clean:
 	rm libr3.a src/*.o
---
+++ src/spec.c
@@ -0,0 +1,154 @@
+#include "r3.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <assert.h>
+
+struct r3_spec *r3_create_cuboid_spec() {
+	const float colors[3*24] = {
+		// Red
+		1.0, 0.0, 0.0,
+		1.0, 0.0, 0.0,
+		1.0, 0.0, 0.0,
+		1.0, 0.0, 0.0,
+		// Cyan
+		0.0, 1.0, 1.0,
+		0.0, 1.0, 1.0,
+		0.0, 1.0, 1.0,
+		0.0, 1.0, 1.0,
+		// Green
+		0.0, 1.0, 0.0,
+		0.0, 1.0, 0.0,
+		0.0, 1.0, 0.0,
+		0.0, 1.0, 0.0,
+		// Magenta
+		1.0, 0.0, 1.0,
+		1.0, 0.0, 1.0,
+		1.0, 0.0, 1.0,
+		1.0, 0.0, 1.0,
+		// Blue
+		0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0,
+		// Yellow
+		1.0, 1.0, 0.0,
+		1.0, 1.0, 0.0,
+		1.0, 1.0, 0.0,
+		1.0, 1.0, 0.0,
+	};
+	const float positions[3*24] = {
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f,  0.5f,
+		0.5f, -0.5f,  0.5f,
+		0.5f, -0.5f, -0.5f,
+		-0.5f,  0.5f, -0.5f,
+		-0.5f,  0.5f,  0.5f,
+		0.5f,  0.5f,  0.5f,
+		0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f,  0.5f, -0.5f,
+		0.5f,  0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, 0.5f,
+		-0.5f,  0.5f, 0.5f,
+		0.5f,  0.5f, 0.5f, 
+		0.5f, -0.5f, 0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f,  0.5f,
+		0.5f,  0.5f,  0.5f,
+		0.5f,  0.5f, -0.5f,
+	};
+	const float normals[3*24] = {
+		0.0f, -1.0f, 0.0f,
+		0.0f, -1.0f, 0.0f,
+		0.0f, -1.0f, 0.0f,
+		0.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, -1.0f,
+		0.0f, 0.0f, -1.0f,
+		0.0f, 0.0f, -1.0f,
+		0.0f, 0.0f, -1.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+		-1.0f, 0.0f, 0.0f,
+		-1.0f, 0.0f, 0.0f,
+		-1.0f, 0.0f, 0.0f,
+		-1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+	};
+	const float texcoords[2*24] = {
+		0.0f, 0.0f,
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		1.0f, 0.0f,
+		1.0f, 0.0f,
+		1.0f, 1.0f,
+		0.0f, 1.0f,
+		0.0f, 0.0f,
+		0.0f, 0.0f,
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		1.0f, 0.0f,
+		0.0f, 0.0f,
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		1.0f, 0.0f,
+		0.0f, 0.0f,
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		1.0f, 0.0f,
+		1.0f, 0.0f,
+		1.0f, 1.0f,
+		0.0f, 1.0f,
+		0.0f, 0.0f,
+	};
+	const unsigned short int indices[36] = {
+		0, 2, 1,
+		0, 3, 2, 
+		4, 5, 6,
+		4, 6, 7,
+		8, 9, 10,
+		8, 10, 11, 
+		12, 15, 14,
+		12, 14, 13, 
+		16, 17, 18,
+		16, 18, 19, 
+		20, 23, 22,
+		20, 22, 21
+	};
+
+	char *buf = malloc(sizeof(struct r3_spec) + 24 * sizeof(struct r3_pcnt) + 36 * sizeof(unsigned short int));
+	struct r3_spec *spec = (void*)buf;
+	spec->verts.tag = R3_VERTS_PCNT;
+	spec->verts.len = 24;
+	spec->verts.data = (void*) (sizeof(struct r3_spec) + buf);
+	spec->indices.tag = R3_INDICES_USHORT;
+	spec->indices.len = 36;
+	spec->indices.data = (void*)(24 * sizeof(struct r3_pcnt) + sizeof(struct r3_spec) + buf);
+
+	for (int i = 0; i < 24; i++) {
+		spec->verts.pcnt[i] = (struct r3_pcnt) {
+			.position = _v3f(positions[i*3+0], positions[i*3+1], positions[i*3+2]),
+			.color = _v3f(colors[i*3+0], colors[i*3+1], colors[i*3+2]),
+			.normal = _v3f(normals[i*3+0], normals[i*3+1], normals[i*3+2]),
+			.texcoord = _v2f(texcoords[i*2+0], texcoords[i*2+1]),
+		};
+	}
+
+	memcpy(spec->indices.data, indices, sizeof(unsigned int short) * 36);
+
+	return spec;
+}
