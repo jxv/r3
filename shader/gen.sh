232a1d6c7eb60d813f8c3e2eb610071eafee78b5
jxv <jxv@hush.com>
2015-05-27 21:50:33 -0700

    More shaders; Improve shader header generator

--- shader/gen.sh
+++ shader/gen.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
-xxd -i shader/cell.vert > shader/cell.vert.h
-xxd -i shader/cell.frag > shader/cell.frag.h
-xxd -i shader/normal.frag > shader/normal.frag.h
-xxd -i shader/normal.vert > shader/normal.vert.h
-xxd -i shader/color.vert > shader/color.vert.h
-xxd -i shader/color.frag > shader/color.frag.h
-xxd -i shader/texture.vert > shader/texture.vert.h
-xxd -i shader/texture.frag > shader/texture.frag.h
-xxd -i shader/color_normal_texture.vert > shader/color_normal_texture.vert.h
-xxd -i shader/color_normal_texture.frag > shader/color_normal_texture.frag.h
+
+for i in `find shader | grep "\.frag" | grep -v "\.h"`
+do
+	xxd -i $i > $i.h
+done
+
+for i in `find shader | grep "\.vert" | grep -v "\.h"`
+do
+	xxd -i $i > $i.h
+done
